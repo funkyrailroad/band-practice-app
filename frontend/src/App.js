2f5adae6b6eb9ccda817d5cb04903b5cb57705d4
Jake Atwell <jake.atwell@gmail.com>
2024-08-08 19:51:36 -0400

    Show waveform

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Waveform from './waveform';
+import pianoClip from './piano.mp3'
+
 
 function App() {
   return (
     <>
       <h1>Band Practice App</h1>
+      <Waveform audio={pianoClip} />
     </>
   );
 }
